9d34df18d88baf0c3c9d4750c7d0f2e9d05837ae
admiretan <54794176+admiretan@users.noreply.github.com>
2019-12-24 10:28:53 +0800

    Update DynamicMemoryAllocatation.c

--- DynamicMemoryAllocatation.c
+++ DynamicMemoryAllocatation.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+  void check(int *p);
+  int *p1,i;
+  p1=(int *)(malloc(5*sizeof(int)));
+  for(i=0;i<5;i++)
+    scanf("%d",p1+i);
+  check(p1);
+  return 0;
+}
+
+void check(int *p)
+{
+  int i;
+  for(i=0;i<5;i++)
+  {
+    if(*(p+i)<60)
+      printf("%d\t",*(p+i));
+  }
+}
